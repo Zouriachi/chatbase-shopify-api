6891d68bb8118e66ffe226d90eb73413db11b985
Zouriachi <zakaria.ouriachi@gmail.com>
2025-04-12 12:28:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,6 @@ app.register_blueprint(products.products_bp)
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
