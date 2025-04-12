0b8ceaa296ec34e173368f870095a9ca730e5a74
Zouriachi <zakaria.ouriachi@gmail.com>
2025-04-12 11:27:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
-from routes import all_routes
+from routes import order_status
+from routes import products
 
 app = Flask(__name__)
 
-for route in all_routes:
-    app.register_blueprint(route)
+# Enregistrement des blueprints
+app.register_blueprint(order_status.order_bp)
+app.register_blueprint(products.products_bp)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
