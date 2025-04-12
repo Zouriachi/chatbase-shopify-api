d1b42eff5660b040daac008b36bba3eec608379b
Zouriachi <zakaria.ouriachi@gmail.com>
2025-04-12 11:39:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from routes import order_status
+from routes import orders
 from routes import products
 
 app = Flask(__name__)
 
 # Enregistrement des blueprints
-app.register_blueprint(order_status.order_bp)
+app.register_blueprint(orders.order_bp)
 app.register_blueprint(products.products_bp)
 
 if __name__ == "__main__":
