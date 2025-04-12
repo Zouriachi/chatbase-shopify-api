28134098ef91fedcae6cf5d95269e2f091bb92ba
Zouriachi <zakaria.ouriachi@gmail.com>
2025-04-12 11:10:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from routes.commandes import commandes_bp
+from routes import all_routes
 
 app = Flask(__name__)
 
-# Enregistrement des routes
-app.register_blueprint(commandes_bp)
+for route in all_routes:
+    app.register_blueprint(route)
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
